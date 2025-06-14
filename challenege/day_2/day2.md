76fffb846bb7c7d7033405c4e518fde2daa51999
Ashutosh Apurva <apooorva01@gmail.com>
2025-06-14 03:42:24 +0000

    add day 2 challenge

--- challenege/day_2/day2.md
+++ challenege/day_2/day2.md
@@ -1,6 +1,6 @@
 # Day 2 Challenge 
-## Infinite Loop with break test
+
+## Infinite Loop and break test with count to prevent infinite loop in case of persistent failure.
 
  ###  In this challenge, you write an infinite loop and use a test to break out after a specific number of tasks have been done.
 
- 
--- challenege/day_2/infinite_loop.sh
+++ challenege/day_2/infinite_loop.sh
@@ -1,20 +1,35 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+# Infinte loop with break test
 # Real world scenario:
 # Wait til a service readiness!
 
+MAX_RETRIES=20
+RETRY_COUNT=0
+
+
 while true; do
     
-     echo "Checking the serice readiness!..."
-     curl -i https://www.myservice.net/health | grep "200 OK" > /dev/null
-     if [ $? -eq 0 ]; then
+     echo "Checking the service readiness!..."
+     HTTP_STATUS=$(curl -s -o /dev/null -w  "%{http_code}" https://www.ashutoshapurva.com)
+
+     if [[ "$HTTP_STATUS" == "200" ]]; then
         echo "Service is ready!"
         break
     else
-        echo "Service is not ready yet.Retrying in 5 seconds.....!"
+        echo "Service is not ready yet. Retrying in 5 seconds.....!"
         sleep 5
+        if [[ $RETRY_COUNT -ge $MAX_RETRIES ]]; then
+            echo "Max retries reached. Exiting..."
+            exit 1
+        fi
+
     fi
 done
+
+
 
 
 
