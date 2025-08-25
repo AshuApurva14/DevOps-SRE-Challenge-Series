df0e914b6379ca56a498197508b89a5757d3638b
Ashutosh Apurva <apooorva01@gmail.com>
2025-08-25 02:48:31 +0000

    my challenge

---
+++ scripts/auto_dir.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo -e " Do you want to Create  the DevOps SRE Challege directory structure!\n Let's Create it, please enter day no ...."
+
+# Challenges faced - on above line related to \n
+ # Fix - 
+
+
+DAY_COUNT=$1
+DIR_PATH=/workspaces/DevOps-SRE-Challenge-Series/DevOps_SRE_Challenge
+
+for dir in $DAY_COUNT
+do 
+
+   mkdir -p $DIR_PATH/Day_$dir
+   echo "Directory created successfully at $DIR_PATH"
+
+done
