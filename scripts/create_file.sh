e8d714a778643296c7f8fa3bf6789bb33e807941
Ashutosh Apurva <apooorva01@gmail.com>
2025-09-08 15:23:19 +0000

    feat:create file for day 5 task

---
+++ scripts/create_file.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+no_of_days=$1
+
+create_files() {
+
+   file_path=/workspaces/DevOps-SRE-Challenge-Series/DevOps_SRE_Challenge
+   
+
+   create_file="$(touch $file_path/Day_$no_of_days/challenge.md $file_path/Day_$no_of_days/solution.md)"
+
+   echo "Files successfully created"
+
+}
+
+create_files
